b0d2e2dc63cf1f7315ca03039cc4faca581c0deb
VinniLP <dellvinni@gmail.com>
2026-01-01 21:27:01 +0530

    GFG20260101.1

---
+++ 2026-01-01 Intersection-in-Y-Shaped-Lists.cpp
@@ -0,0 +1,35 @@
+/*
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    Node* intersectPoint(Node* head1, Node* head2) {
+        //  Code Here
+    Node *tempA = head1; 
+        Node *tempB = head2; 
+        
+        while(tempA != tempB){
+            if(tempA == nullptr){
+                tempA = head1; 
+            }
+            else tempA = tempA->next; 
+            
+            if(tempB == nullptr){
+                tempB = head2; 
+            }
+            else tempB = tempB->next;
+        }
+        return tempA; 
+        
+    }
+};
